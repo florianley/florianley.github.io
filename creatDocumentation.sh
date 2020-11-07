dfacb10b325763a3782e0ee5153fc831cdc8fc2c
Florian LEY <florian.ley@worldline.com>
2020-11-07 12:30:45 +0000

    cleanup and update deployment script

--- creatDocumentation.sh
+++ creatDocumentation.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+echo "html data cleaning"
+rm *.html
+
+echo "images cleanup file"
+rm -rf img/
+
+
 echo "asciidoctor -r asciidoctor-diagram source/designPattern.adoc -o designPattern.html -a stylesheet=stylesheet.css"
 asciidoctor -r asciidoctor-diagram source/designPattern.adoc -o designPattern.html -a stylesheet=stylesheet.css
-
 
 echo "asciidoctor -r asciidoctor-diagram source/index.adoc -o index.html -a stylesheet=stylesheet.css"
 asciidoctor -r asciidoctor-diagram source/index.adoc -o index.html -a stylesheet=stylesheet.css
@@ -10,3 +16,7 @@ asciidoctor -r asciidoctor-diagram source/index.adoc -o index.html -a stylesheet
 echo "asciidoctor -r asciidoctor-diagram source/asciidoc.adoc -o asciidoc.html -a stylesheet=stylesheet.css"
 asciidoctor -r asciidoctor-diagram source/asciidoc.adoc -o asciidoc.html -a stylesheet=stylesheet.css
 
+echo "images integration in images file"
+cp source/img/* img/
+
+echo "html documentation ready to be deployed to the world"
