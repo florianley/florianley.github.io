2db3e127d9d37f95b180be95bb34ecb5569ed774
Florian LEY <florian.ley@worldline.com>
2020-11-07 19:26:48 +0000

    update index  and script

--- creatDocumentation.sh
+++ creatDocumentation.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-
+green=`tput setaf 2`
+reset=`tput sgr0`
 echo "html data cleaning"
 rm *.html
 
 echo "images cleanup file"
 rm -rf img/
-
-
 
 # shellcheck disable=SC2045
 for file in `ls source/*.adoc`
@@ -14,13 +13,10 @@ do
         printf "integration of : $file --> "
         htmlfile="${file%.adoc}.html"
         asciidoctor -r asciidoctor-diagram $file -o $htmlfile -a stylesheet=stylesheet.css
-        echo "ok"
+        echo "${green}ok${reset}"
 done
 mv source/*.html .
-
 echo "images integration in images file"
-pwd
 mkdir img
 cp source/img/* img/
-
 echo "html documentation ready to be deployed to the world"
